107afda5dc4d10ff8f9bf6f48a0bcff017e0ad86
Riley Kirkham <rileykirkham@Rileys-MacBook-Pro.local>
2020-02-13 18:16:07 -0700

    Added DisplayBoard() using a switch statement. Also implemented InsertLines()

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+//A nicer looking way to implement std::endls.
+void InsertLines(int num){
+    for(int i = 0; i < num; i++){
+        std::cout<<std::endl;
+    }
+}
+
+
 //Initializes 3x3 board of 0s.
 int* CreateBoard(){
     int* arr = new int[9];
@@ -9,6 +17,31 @@ int* CreateBoard(){
     return arr;
 }
 
+//Displays board with code 0 = _ , 1 = X, 2 = O.
+void DisplayBoard(int* table){
+    for(int i = 0; i < 9; i ++){
+        switch (table[i])
+        {
+        case 0:
+            std::cout <<"_";
+            break;
+        
+        case 1:
+            std::cout<< "X";
+            break;
+
+        case 2:
+            std::cout<<"O";
+            break;
+        
+        default:
+            std::cout<< " error ";
+        }
+        if(i % 3 == 2){
+            InsertLines(1);
+        }
+    }
+}
 int main(){
 	int* table = CreateBoard();
 }
