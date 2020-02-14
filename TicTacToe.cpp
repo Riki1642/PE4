9656d53fd33310c81bdc378c9c71a8c9e4b75999
Riley Kirkham <rileykirkham@Rileys-MacBook-Pro.local>
2020-02-13 18:18:47 -0700

    Added DisplayBoard() using if statements

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -9,6 +9,33 @@ int* CreateBoard(){
     return arr;
 }
 
+
+
+//Displays board with code 0 = _ , 1 = X, 2 = O.
+void DisplayBoard(int* table){
+    for(int i = 0; i < 9; i ++){
+        
+        if(table[i] == 0){
+            std::cout <<"_";
+            break;
+        }
+        else if(table[i] == 1){
+            std::cout<< "X";
+            break;
+        }
+        else if(table[i] == 2){
+            std::cout<<"O";
+            break;
+        }
+        else{
+            std::cout<< " error ";
+        }
+        if(i % 3 == 2){
+            std::cout<<std::endl;
+        }
+    }
+}
+
 int main(){
 	int* table = CreateBoard();
 }
