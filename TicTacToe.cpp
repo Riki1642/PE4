4289a91ed3d0058196a5a6ac34884a41dd383383
Riley Kirkham <rileykirkham@Rileys-MacBook-Pro.local>
2020-02-13 18:25:22 -0700

    Added GetPlayerChoice() and PrintLocationBoard()

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -44,6 +44,55 @@ void DisplayBoard(int* table){
     }
 }
 
+//Prints the board that shows number locations (1-9 grid).
+void PrintLocationBoard(){
+    for(int i = 1; i < 10; i++){
+        std::cout<<i;
+        if(i % 3 == 0){
+            InsertLines(1);
+        }
+    }
+}
+
+
+//Gets the location the player wants to place thier mark.
+int GetPlayerChoice(int* table){
+    int choice = -1;
+    InsertLines(1);
+    std::cout<<"Choose Where to play. Input Correspoinding number.";
+    InsertLines(2);
+    bool valid_guess = false;
+    while(!valid_guess){
+        PrintLocationBoard();
+        InsertLines(1);
+        DisplayBoard(table);
+        InsertLines(1);
+        std::cout<<"Input: ";
+        std::cin >> choice;
+        choice = choice -1; // Displayed table was range 1-9 while we need it in terms of 0-8.
+        if(choice >= 0 && choice < 9){
+            if(table[choice] == 0){ //If Space empty
+                valid_guess = true;
+            }
+            else{
+                InsertLines(1);
+                std::cout<<"This square has already been chosen."<<std::endl<<"Please Choose Again";
+                InsertLines(1);
+            }
+        }
+        else{
+            InsertLines(1);
+            std::cout<<"Your input was out of range."<<std::endl<< "Please choose a number between 1-9";
+            InsertLines(1);
+        }
+        InsertLines(1);
+    }
+    return choice;
+}
+
+
+
+
 int main(){
 	int* table = CreateBoard();
 }
